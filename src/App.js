a2c8c380792dfa1cb37d9830aa9e5f0d0d40c0a9
douknowccy <twkccy1990@gmail.com>
2020-11-11 17:06:14 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+
+const cliendID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState("");
+
+  const fetchIamges = async () => {
+    setLoading(true);
+    let url;
+    const urlQuery = `&query=${query}`;
+    const urlPage = `&page=${page}`;
+    //protect empty search input by fetching default image
+    if (query) {
+      url = `${searchUrl}${cliendID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${cliendID}${urlPage}`;
+    }
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
+      });
+
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+  //render page by page changed
+  useEffect(() => {
+    fetchIamges();
+    // eslint-disable-next-line
+  }, [page]);
+  //listen to infinite page by scorll down
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
+    return () => window.removeEventListener("scroll", event);
+    // eslint-disable-next-line
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //set search phtots to first page
+    setPage(1);
+  };
+
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input
+            type="text"
+            placeholder="search"
+            className="form-input"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button className="submit-btn" type="submit" onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((image, index) => {
+            return <Photo key={index} {...image} />;
+          })}
+        </div>
+        {loading && <h2 className="loading">Loading...</h2>}
+      </section>
+    </main>
+  );
+}
+
+export default App;
